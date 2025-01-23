5a0ed599cc58c91a7ba84b308981b6e496566424
markbakos <markbakosss@gmail.com>
2025-01-23 23:16:54 +0100

    Updated text

--- client/src/app/components/Features.tsx
+++ client/src/app/components/Features.tsx
@@ -25,7 +25,7 @@ export default function Features() {
                 <div className="lg:text-center">
                     <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">Features</h2>
                     <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                        Why Choose ImageLocator?
+                        Why Use LocationGuesser?
                     </p>
                 </div>
 
