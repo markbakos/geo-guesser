e9141ced7760656a295dd3d97f70e40854641695
markbakos <markbakosss@gmail.com>
2025-02-12 18:19:32 +0100

    Added coordinate normalization to preprocessing

--- model/data_generator.py
+++ model/data_generator.py
@@ -14,13 +14,26 @@ class DataGenerator(utils.Sequence):
         self.shuffle = shuffle
         self.indexes = np.arange(len(self.metadata))
 
+        self.lat_min, self.lat_max = self.metadata['latitude'].min(), self.metadata['latitude'].max()
+        self.lon_min, self.lon_max = self.metadata['longitude'].min(), self.metadata['longitude'].max()
+
         self.city_encoder = LabelEncoder()
         self.metadata['city_encoded'] = self.city_encoder.fit_transform(self.metadata['city'])
 
+        stats = {
+            'lat_min': self.lat_min,
+            'lat_max': self.lat_max,
+            'lon_min': self.lon_min,
+            'lon_max': self.lon_max
+        }
+
         encoder_path = Path("model/checkpoints/city_encoder.pkl")
+        stats_path = Path("model/checkpoints/coordinate_stats.pkl")
         encoder_path.parent.mkdir(exist_ok=True, parents=True)
         with open(encoder_path, 'wb') as f:
             pickle.dump(self.city_encoder, f)
+        with open(stats_path, 'wb') as f:
+            pickle.dump(stats, f)
 
         if self.augment:
             self.aug_layer = Sequential([
@@ -32,6 +45,11 @@ class DataGenerator(utils.Sequence):
             ])
 
         self.on_epoch_end()
+
+    def normalize_coordinates(self, lat, lon):
+        lat_norm = (lat - self.lat_min) / (self.lat_max - self.lat_min)
+        lon_norm = (lon - self.lon_min) / (self.lon_max - self.lon_min)
+        return np.array([lat_norm, lon_norm])
 
     def __len__(self):
         return int(np.ceil(len(self.metadata) / self.batch_size))
@@ -56,7 +74,7 @@ class DataGenerator(utils.Sequence):
             img = applications.efficientnet.preprocess_input(img)
 
             batch_x[i] = img
-            batch_y_coords[i] = [row['latitude'], row['longitude']]
+            batch_y_coords[i] = self.normalize_coordinates(row['latitude'], row['longitude'])
             batch_y_cities[i] = utils.to_categorical(row['city_encoded'], num_classes=len(self.city_encoder.classes_))
 
         return batch_x, {
